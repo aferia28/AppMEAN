6f3a995cd85bdf96082a49013ec77e6ffa07040a
Adrian Feria <adri.gp7@gmail.com>
2016-02-19 23:56:04 +0100

    Product Design

--- app.js
+++ app.js
@@ -5,87 +5,8 @@ var express = require('express');
 var path = require('path');
 var mongoose  = require('mongoose');
 
-//var nodemailer = require("nodemailer");
 
 var app = express();
-/*
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "proyectomean@gmail.com",
-        pass: "proyectodaw2a"
-    }
-});
-
-var rand,mailOptions,host,link;
-
-app.get('/send/:email',function(req,res){
-
-    var data = req.params.email;
-    console.log('req.body: ' + data);
-
-    rand=Math.floor((Math.random() * 100) + 54);
-    host=req.get('host');
-    link="http://"+req.get('host')+"/verify?id="+rand;
-    mailOptions={
-        to : data,
-        subject : "Please confirm your Email account",
-        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
-            console.log(error);
-        res.end("error");
-     }else{
-            console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
-	});
-});
-
-app.get('/verify',function(req,res){
-
-	console.log(req.protocol+":/"+req.get('host'));
-	if((req.protocol+"://"+req.get('host'))==("http://"+host))
-	{
-	    console.log("Domain is matched. Information is from Authentic email");
-	    if(req.query.id==rand)
-	    {
-	        console.log("email is verified");
-          console.log('Email validation : ' + mailOptions.to);
-
-	        res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
-
-          Persona.find('email', mailOptions.to, function(err, persona) {
-            if(err){
-                res.send(err);
-            }
-            else{
-              persona.verified = true;
-
-              persona.save(function(err) {
-              if(err) return err.status(500).send(err.message);
-              else {console.log('persona actualizada correctamente');res.status(200).jsonp(persona)}
-            });
-              console.log('Persona verificada correctamente');
-              res.json(persona);
-            }
-          })
-          //complete singup aquí. Persona.verified = true;
-	    }
-	    else
-	    {
-	        console.log("email is not verified");
-	        res.end("<h1>Bad Request</h1>");
-	    }
-	}
-	else
-	{
-	    res.end("<h1>Request is from unknown source");
-	}
-});*/
-
 //Iniciamos express
 
 mongoose.connect('mongodb://localhost:27017/DBpersonas', function(error){
--- public/js/controllers/productSearchController.js
+++ public/js/controllers/productSearchController.js
@@ -1,8 +1,12 @@
 app.controller('productSearcherController', ['$scope', '$http','$rootScope','$routeParams', function($scope, $http, $rootScope,$routeParams) {
 
+			$scope.pageClass = 'page-product';
+			$("aside").fadeOut("slow", function() {
+				$("#content").removeClass("col-sm-10").addClass("col-sm-12");
+			});
+
 
 			var codeWine;
-
 			console.log($routeParams.wineCode);
 
 			codeWine = $routeParams.wineCode;
@@ -10,7 +14,25 @@ app.controller('productSearcherController', ['$scope', '$http','$rootScope','$ro
 			$http.get('http://api.snooth.com/wine/?akey=mi24ey8gwq286zony5uw51ghphnjed0yz0h6hpjs6l7rrr17&id='+codeWine)
 			.success(function(data){
 				console.log(data);
+				console.log();
 				$scope.product = data.wines[0];
+				if(data.wines[0].type == 'Red Wine')
+				{
+					$scope.type = 'red';
+					console.log('Red wine returend');
+					$('#product-header').addClass('product-header-red');
+
+				}else if(data.wines[0].type == 'White Wine'){
+					$scope.type = 'white';
+					console.log('White wine returend');
+					$('#product-header').addClass('product-header-white');
+
+				}else if(data.wines[0].type = 'Rose Wine'){
+					$scope.type = 'rose';
+					console.log('Rose wine returend');
+					$('#product-header').addClass('product-header-rose');
+
+				}
 			})
 
 
